bc9b4ddeba1a936f26f415841f8a2aec2ce0b8c7
kartikeyas-x <115060334+kartikeyas-x@users.noreply.github.com>
2024-12-18 16:58:47 +0530

    Update getBaseUrl.js

--- client/src/utils/getBaseUrl.js
+++ client/src/utils/getBaseUrl.js
@@ -1,4 +1,4 @@
 const getBaseUrl = () => {
-  return "https://carya-ten.vercel.app/" || "http://localhost:5001";
+  return "https://carya-z3ry-sever.vercel.app/" || "http://localhost:5001";
 };
 export default getBaseUrl;
