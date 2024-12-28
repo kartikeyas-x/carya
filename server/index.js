139625ebd91994bba950a4b6d23093338a6cc7a3
kartikeyas-x <115060334+kartikeyas-x@users.noreply.github.com>
2024-12-28 10:35:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,8 +12,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5174',  // Your frontend origin
-    'http://localhost:5176','https://carya-ten.vercel.app/',],
+    origin: ['https://carya-ten.vercel.app/',],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
